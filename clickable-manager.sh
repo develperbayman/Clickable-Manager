a938dc3cd7f1c835683479af12648a8c2d67365d
develperbayman <develperbayman@users.noreply.github.com>
2023-01-28 12:21:10 +0800

    Add files via upload

---
+++ clickable-manager.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# utilitymenu.sh - A sample shell script to display menus on screen
+# Store menu options selected by the user
+INPUT=/tmp/menu.sh.$$
+
+# Storage file for displaying cal and date command output
+OUTPUT=/tmp/output.sh.$$
+
+# get text editor or fall back to vi_editor
+vi_editor=${EDITOR-vi}
+
+# trap and delete temp files
+trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
+
+#
+# Purpose - display output using msgbox 
+#  $1 -> set msgbox height
+#  $2 -> set msgbox width
+#  $3 -> set msgbox title
+#
+function display_output(){
+	local h=${1-10}			# box height default 10
+	local w=${2-41} 		# box width default 41
+	local t=${3-Output} 	# box title 
+	dialog --backtitle "Clickable-Manager" --title "${t}" --clear --msgbox "$(<$OUTPUT)" ${h} ${w}
+}
+#
+# Purpose - display current system date & time
+#
+function show_date(){
+	echo "Today is $(date) @ $(hostname -f)." >$OUTPUT
+    display_output 6 60 "Date and Time"
+}
+#
+# Purpose - display a calendar
+#
+function show_calendar(){
+	cal >$OUTPUT
+	display_output 13 25 "Calendar"
+}
+#
+# set infinite loop
+#
+while true
+do
+
+### display main menu ###
+dialog --clear  --help-button --backtitle "Clickable-Manager" \
+--title "[ M A I N - M E N U ]" \
+--menu "" 15 50 4 \
+ClickableCreate "Create a Clickable project" \
+Calendar "Displays a calendar" \
+Terminal "Start a terminal emulator" \
+Exit "Exit to the shell" 2>"${INPUT}"
+
+menuitem=$(<"${INPUT}")
+
+
+# make decsion 
+case $menuitem in
+	ClickableCreate) exec clickable create;;
+	Calendar) show_calendar;;
+	Editor) show x-terminal-emulator -e "./application_name [arguments]" &;;
+	Exit) echo "Bye"; break;;
+esac
+
+done
+
+# if temp files found, delete em
+[ -f $OUTPUT ] && rm $OUTPUT
+[ -f $INPUT ] && rm $INPUT
